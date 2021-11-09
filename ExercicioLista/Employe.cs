3a401621d31ce57c4f1dfa0027bbfbdf492b9b4d
Arthur Henrique <arthurhenriquedepaula@gmail.com>
2021-11-09 15:28:52 -0300

    Estudo simples de Listagem em C#

---
+++ ExercicioLista/Employe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLista
+{
+    class Employe
+    {
+        public int id;
+        public string name;
+        public double salary;
+
+        public Employe(int Id, string Name, double Salary)
+        {
+            id = Id;
+            name = Name;
+            salary = Salary;
+        }
+
+        public void SalaryIncrease(double porcentage)
+        {
+            salary = salary + ((salary * porcentage) / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{id}, {name}, {salary}";
+        } 
+    }
+}
---
+++ ExercicioLista/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ExercicioLista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employe> Lista = new List<Employe>();
+
+            Console.WriteLine("How many employees will be registered?:");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                Console.WriteLine("Employe Id:");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Employe Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Employe Salary:");
+                double salary = double.Parse(Console.ReadLine());
+
+
+                Employe Funcionário = new(id, name, salary);
+                Lista.Add(Funcionário);
+            }
+
+            Console.WriteLine("Enter the Employee id that wil have salary increase:");
+            int LocateId = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the percentage:");
+            double percentage = double.Parse(Console.ReadLine());
+
+            foreach (var item in Lista)
+            {
+                if(item.id == LocateId)
+                {
+                    item.SalaryIncrease(percentage);
+                }
+            }
+
+            Console.WriteLine("Updated list of employees:");
+
+            foreach (var item in Lista)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+
+        }
+    }
+}
